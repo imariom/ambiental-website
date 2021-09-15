87548294402cf349cf02c9619aa195ab4f824062
imariom <mario.moiane@mservertech.com>
2021-09-15 10:41:27 -0700

    Fixed file referencing for github setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,16 +13,15 @@ module.exports = {
         'secondary': '#2F80ED'
       },
       spacing: {
-        '128': '28rem',
-        '144': '32rem'
+        '128': '28rem'
       },
       backgroundImage: {
-        'header-texture-0': "url('/static/images/ross-s-3uTGaHtWCxg-unsplash.jpg')",
-        'header-texture-1': "url('/static/images/andrew-haimerl-mms2ZeTvdM4-unsplash.jpg')",
-        'header-texture-2': "url('/static/images/ocg-saving-the-ocean-DtbOD4_bHA4-unsplash.jpg')",
-        'header-texture-3': "url('/static/images/ocg-saving-the-ocean-rXjzIsQAQ-w-unsplash.jpg')",
+        'header-texture-0': "url('../images/ross-s-3uTGaHtWCxg-unsplash.jpg')",
+        'header-texture-1': "url('../images/andrew-haimerl-mms2ZeTvdM4-unsplash.jpg')",
+        'header-texture-2': "url('../images/ocg-saving-the-ocean-DtbOD4_bHA4-unsplash.jpg')",
+        'header-texture-3': "url('../images/ocg-saving-the-ocean-rXjzIsQAQ-w-unsplash.jpg')",
 
-        'service-header-texture-0': "url('/static/images/christina-wocintechchat-com-faEfWCdOKIg-unsplash.jpg')"
+        'service-header-texture-0': "url('../images/christina-wocintechchat-com-faEfWCdOKIg-unsplash.jpg')"
       }
     },
   },
